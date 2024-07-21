6fb6152d1307f41ae27565654c6d1f89b20dfe0b
Omar Ebrahim Mohamed Khalil <200012464@must.edu.eg>
2024-07-21 15:18:09 +0300

    Question1,2,3

---
+++ Classes/3D_Point.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6.Classes
+{
+    internal class Point3D
+    {
+        #region Q1
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public Point3D(int _x)
+        {
+            X = _x;
+        }
+        public Point3D(int _x, int _y) : this(_x) //Chaining from first Constructor 
+        {
+            X = _x; Y = _y;
+        }
+        public Point3D(int _x, int _y, int _z) : this(_x, _y)//Chaining from Second Constructor 
+        {
+            X = _x; Y = _y; Z = _z;
+        }
+        // Override ToString method
+        public override string ToString()
+        {
+            return $"Point Coordinates: ({X}, {Y}, {Z})";
+        } 
+        #endregion
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,83 @@
-﻿namespace ConsoleApp6
+﻿using ConsoleApp6.Classes;
+using System.Drawing;
+
+namespace ConsoleApp6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q2
+            //Point3D _3D_Point = new _3D_Point(10, 10, 10);
+            //Console.WriteLine(_3D_Point.ToString()); 
+            #endregion
+            #region Q3
+            //Point3D p1;
+            //Point3D p2;
+
+            //Console.WriteLine("Enter 3 Points to P1:");
+            //int x;
+            //int y;
+            //int z;
+            //if (!int.TryParse(Console.ReadLine(), out x))
+            //{
+            //    Console.WriteLine("Invalid Input");
+            //}
+            //if (!int.TryParse(Console.ReadLine(), out y))
+            //{
+            //    Console.WriteLine("Invalid Input");
+            //}
+            //if (!int.TryParse(Console.ReadLine(),out z))
+            //{
+            //    Console.WriteLine("Invalid Input");
+            //}
+            //p1=new Point3D(x, y, z);
+            //Console.WriteLine("Enter 3 Points to P1:");
+            //if (!int.TryParse(Console.ReadLine(), out x))
+            //{
+            //    Console.WriteLine("Invalid Input");
+            //}
+            //if (!int.TryParse(Console.ReadLine(), out y))
+            //{
+            //    Console.WriteLine("Invalid Input");
+            //}
+            //if (!int.TryParse(Console.ReadLine(), out z))
+            //{
+            //    Console.WriteLine("Invalid Input");
+            //}
+            //p2 = new Point3D(x, y, z);
+
+            /*
+             * With Parse
+             */
+            //Console.WriteLine("Enter the point for p1:");
+            //string x=Console.ReadLine();
+            //string y= Console.ReadLine();
+            //string z = Console.ReadLine();
+            //p1 = new Point3D(int.Parse(x), int.Parse(y), int.Parse(z));
+            //Console.WriteLine("Enter the point for p2:");
+            // x=Console.ReadLine();
+            // y= Console.ReadLine();
+            // z = Console.ReadLine();
+            //p2 = new Point3D(int.Parse(x), int.Parse(y), int.Parse(z));
+
+            //with Convert
+            //Console.WriteLine("Enter the point for p1:");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //int y = Convert.ToInt32(Console.ReadLine());
+            //int z = Convert.ToInt32(Console.ReadLine());
+            //p1 = new Point3D(x, y, z);
+            //Console.WriteLine("Enter the point for p2:");
+            // x = Convert.ToInt32(Console.ReadLine());
+            // y = Convert.ToInt32(Console.ReadLine());
+            // z = Convert.ToInt32(Console.ReadLine());
+            //p2 = new Point3D(x, y, z);
+            //Console.WriteLine(p1);
+            //Console.WriteLine(p2);
+
+
+
+            #endregion
         }
     }
 }
