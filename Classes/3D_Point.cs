20d71b175de881d57dd8d406c51c514086cf2619
Omar Ebrahim Mohamed Khalil <200012464@must.edu.eg>
2024-07-22 23:24:23 +0300

    Question4,5,6

--- Classes/3D_Point.cs
+++ Classes/3D_Point.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp6.Classes
 {
-    internal class Point3D
+    internal class Point3D:IComparable,ICloneable
     {
         #region Q1
         public int X { get; set; }
@@ -25,11 +27,31 @@ namespace ConsoleApp6.Classes
         {
             X = _x; Y = _y; Z = _z;
         }
+        public static bool operator ==(Point3D p1, Point3D p2)
+        {
+            return p1.X == p2.X && p1.Y == p2.Y && p1.Z == p2.Z;
+        }
+        public static bool operator !=(Point3D p1, Point3D p2)
+        {
+            return p1.X != p2.X && p1.Y != p2.Y && p1.Z != p2.Z;
+        }
         // Override ToString method
         public override string ToString()
         {
             return $"Point Coordinates: ({X}, {Y}, {Z})";
-        } 
+        }
+
+        public int CompareTo(object? obj)
+        {
+            Point3D p = (Point3D)obj;
+            if (X != p.X) { return X.CompareTo(p.X); }
+            else {return Y.CompareTo(p.Y); }
+        }
+
+        public object Clone()
+        {
+            return new Point3D(X, Y, Z);
+        }
         #endregion
 
     }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp6
     {
         static void Main(string[] args)
         {
+            #region First Project
             #region Q2
             //Point3D _3D_Point = new _3D_Point(10, 10, 10);
             //Console.WriteLine(_3D_Point.ToString()); 
@@ -62,26 +63,59 @@ namespace ConsoleApp6
             //p2 = new Point3D(int.Parse(x), int.Parse(y), int.Parse(z));
 
             //with Convert
-            Point3D p1;
-            Point3D p2;
-            Console.WriteLine("Enter the point for p1:");
-            int x = Convert.ToInt32(Console.ReadLine());
-            int y = Convert.ToInt32(Console.ReadLine());
-            int z = Convert.ToInt32(Console.ReadLine());
-            p1 = new Point3D(x, y, z);
-            Console.WriteLine("Enter the point for p2:");
-            x = Convert.ToInt32(Console.ReadLine());
-            y = Convert.ToInt32(Console.ReadLine());
-            z = Convert.ToInt32(Console.ReadLine());
-            p2 = new Point3D(x, y, z);
-            Console.WriteLine(p1);
-            Console.WriteLine(p2);
+            //Point3D p1;
+            //Point3D p2;
+            //Console.WriteLine("Enter the point for p1:");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //int y = Convert.ToInt32(Console.ReadLine());
+            //int z = Convert.ToInt32(Console.ReadLine());
+            //p1 = new Point3D(x, y, z);
+            //Console.WriteLine("Enter the point for p2:");
+            //x = Convert.ToInt32(Console.ReadLine());
+            //y = Convert.ToInt32(Console.ReadLine());
+            //z = Convert.ToInt32(Console.ReadLine());
+            //p2 = new Point3D(x, y, z);
+            //Console.WriteLine(p1);
+            //Console.WriteLine(p2);
+            #region q4
+            //In this way it will not work
+            //if (p1 == p2) { Console.WriteLine("It works"); }
+            //In this way it will work
+            //if (p1 == p2) { Console.WriteLine("It works"); }
+            #endregion
+            #region q5
+            //Point3D[] points = new Point3D[]
+            //{
+            //new Point3D(1, 2, 3),
+            //new Point3D(3, 2, 1),
+            //new Point3D(2, 3, 1),
+            //new Point3D(1, 1, 1),
+            //new Point3D(2, 2, 2)
+            //};
 
+            //    Array.Sort(points);
 
-
+            //foreach (var point in points)
+            //{
+            //    Console.WriteLine(point);
+            //}
 
 
             #endregion
+            #region q6
+            //Point3D p1 = new Point3D(5, 5, 5);
+            //Point3D clone =(Point3D) p1.Clone();
+            //Console.WriteLine($"\nOriginal: {p1}");
+            //Console.WriteLine($"Clone: {clone}");
+            #endregion 
+            #endregion
+            #region MyRegion
+
+            #endregion
+
+
+            #endregion
+
         }
     }
 }
